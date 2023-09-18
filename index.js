0f6cb99a7165d5e3607980c039b6ee84a750e78a
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-09-18 22:02:46 +0200

    [NEW] sacar el porcentaje medio de cada vitamina

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const fetchAsyncData = async () => {
         // maxProteine(result);
         // donutCalories(result);
         // donutCarbohydrate(result);
-        carbohydrateAverage(result);
+        // carbohydrateAverage(result);
+        // saturatedFatSum(result);
+        averageVitamine(result);
     } catch (error){
         console.log(error.message)
     }
@@ -111,4 +113,70 @@ const carbohydrateAverage = (result) => {
     let total = calories.reduce((a, b) => a + b, 0);
     const average = total / calories.length;
     console.log(`La media de calorías de todos los donuts es: ${average}`);
+}
+
+const saturatedFatSum = (result) => {
+
+    const saturatedFat = result.items.item.map(element => Number(element.nutrition_facts.nutrition.fat.fat_type.saturated.substring(0, element.nutrition_facts.nutrition.fat.fat_type.saturated.length - 1)));
+    let total = saturatedFat.reduce((a, b) => a + b, 0);
+    console.log(`La suma de las grasas saturadas de todos los donuts es: ${total}`);
+}
+
+const averageVitamine = (result) => {
+    let vitaminASum = 0;
+    let vitaminCSum = 0;
+    let vitaminCalciumSum = 0;
+    let vitaminIronSum = 0;
+    const saturatedFat = result.items.item.map(element =>  {
+        let totalVitamines = element.nutrition_facts.nutrition.vitamines.map(vitamine => {
+            if(vitamine.type === "Vitamin A"){
+                if(vitamine.percent.substring(vitamine.percent.length - 1, vitamine.percent.length) == '%'){
+                vitaminASum = vitaminASum + Number(vitamine.percent.substring(0, vitamine.percent.length - 1))
+                }
+                else{
+                    vitaminASum = vitaminASum + Number(vitamine.percent);
+                }
+                return vitaminASum;
+            }
+            else if(vitamine.type === "Vitamin C"){
+                if(vitamine.percent.substring(vitamine.percent.length - 1, vitamine.percent.length) == '%'){
+                vitaminCSum = vitaminCSum + Number(vitamine.percent.substring(0, vitamine.percent.length - 1))
+                }
+                else{
+                    vitaminCSum = vitaminCSum + Number(vitamine.percent);
+                }
+                return vitaminCSum;
+            }
+            else if(vitamine.type === "Calcium"){
+                if(vitamine.percent.substring(vitamine.percent.length - 1, vitamine.percent.length) == '%'){
+                vitaminCalciumSum = vitaminCalciumSum + Number(vitamine.percent.substring(0, vitamine.percent.length - 1))
+                }
+                else{
+                    vitaminCalciumSum = vitaminCalciumSum + Number(vitamine.percent);
+                }
+                return vitaminCalciumSum;
+            }
+            else if(vitamine.type === "Iron"){
+                if(vitamine.percent.substring(vitamine.percent.length - 1, vitamine.percent.length) == '%'){
+                vitaminIronSum = vitaminIronSum + Number(vitamine.percent.substring(0, vitamine.percent.length - 1))
+                }
+                else{
+                    vitaminIronSum = vitaminIronSum + Number(vitamine.percent);
+                }
+                return vitaminIronSum;
+            }
+        })
+       return totalVitamines;
+    });
+
+
+
+    let totalVitaminA = (saturatedFat[4][0] / 5);
+    let totalVitaminC = (saturatedFat[4][1] / 5);
+    let totalVitaminCalcium = (saturatedFat[4][2] / 5);
+    let totalVitaminIron = (saturatedFat[4][3] / 5);
+    console.log(`Porcentaje medio de la vitamina A es ${totalVitaminA}%`) 
+    console.log(`Porcentaje medio de la vitamina C es ${totalVitaminC}%`) 
+    console.log(`Porcentaje medio de la vitamina Calcium es ${totalVitaminCalcium}%`) 
+    console.log(`Porcentaje medio de la vitamina Iron es ${totalVitaminIron}%`) 
 }
