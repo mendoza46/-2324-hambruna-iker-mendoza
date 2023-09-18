f42275696b53e161fc65a4196e486de06d936b94
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-09-18 13:15:58 +0200

    [NEW] donut with more sugar

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const getAllDonuts = async () => {
 
 const fetchAsyncData = async () => {
     try{
-        const result = await getAllDonuts()
-        // console.log(result.items);
+        const result = await getAllDonuts();
+        maxSugar(result);
     } catch (error){
         console.log(error.message)
     }
@@ -14,3 +14,17 @@ const fetchAsyncData = async () => {
 
 fetchAsyncData();
 
+const maxSugar = (result) => {
+    const resultado = result.items.item.map(element => {
+        const sugarNumber = element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars.substring(0, 2)
+        const stringNumber = Number(sugarNumber);
+        return stringNumber;
+    })
+
+    const sugarMax = Math.max(...resultado);
+
+    const findNumber = result.items.item.filter(element => sugarMax == element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars.substring(0, 2));
+    
+    console.log(`El donut con mas azúcar es ${findNumber[0].name}`);
+    
+}
